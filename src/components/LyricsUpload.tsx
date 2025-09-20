9369b609cda240e4b95618b426d16f3461411c63
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-21 02:16:20 +0800

    Added responsiveness to most components [add]

--- src/components/LyricsUpload.tsx
+++ src/components/LyricsUpload.tsx
@@ -26,7 +26,7 @@ function LyricsUpload({ onLyricsChange }: LyricsUploadProps) {
       <textarea
         value={lyrics}
         onChange={handleTextAreaChange}
-        className="border boder-2 border-color w-full min-h-60 rounded-lg bg-black-dark text-white placeholder-gray-300 text-sm resize-none p-4"
+        className="border boder-2 border-color w-full min-h-48 sm:min-h-52 md:min-h-60 rounded-lg bg-black-dark text-white placeholder-gray-300 text-sm resize-none p-3 sm:p-4"
         placeholder="For example:
 Just one more tear to cry
 One teardrop from my eye
