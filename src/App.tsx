4a6fae783b14d1dab5d6ee6fbbcaf202645219d2
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-20 19:03:20 +0800

    Routed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Results from "./pages/Results";
+
 function App() {
   return (
-    <main>
-      <h1>Bach or Bot</h1>
-    </main>
+    <div className="bg-color-white">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Landing />} path="/" />
+          <Route element={<Results />} path="/results" />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
