1ee2daebc12e43f022dbe1f180cb39001099b60e
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-20 22:07:07 +0800

    Refactored results page to get API results passed by landing

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,20 +1,27 @@
+import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import ResultsMain from "../components/ResultsMain";
 import Title from "../components/Title";
 
 function Results() {
+  // Use the location api
+  const location = useLocation();
+
+  // To get the API results passed by the landing page
+  const { result, lyrics, fileName } = location.state || {};
+
   return (
     <div>
       {/* Header component (made this a component in case we want to add more moving forward) */}
       <Header />
 
       {/* Main page body */}
-      <main className="text-center">
+      <main className="text-center pt-8 pb-24">
         {/* Title component (made this a component as well just in case we want to add more) */}
         <Title />
 
         {/* Main content container */}
-        <ResultsMain />
+        <ResultsMain result={result} lyrics={lyrics} fileName={fileName} />
       </main>
     </div>
   );
