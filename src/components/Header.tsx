0737f8ad13b3f53f03fe1aec5057a48a2718d415
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-21 00:08:21 +0800

    Added github reference on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <div className="px-12 py-8">
-      <div>Bach or Bot</div>
+    <div className="px-12 py-8 flex justify-between w-full">
+      <Link to="/">Bach or Bot</Link>
+      <a
+        href="https://github.com/krislette/bach-or-bot"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
     </div>
   );
 }
