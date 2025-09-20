e943a29f0a84582c38f1ebc8ae6b1b9bfa8b7f0f
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-20 22:05:33 +0800

    Refactored result pill to display diff scenarios conditionally

--- src/components/ResultPill.tsx
+++ src/components/ResultPill.tsx
@@ -1,10 +1,27 @@
-import { Bot } from "lucide-react";
+import { Bot, UserRoundCheck } from "lucide-react";
 
-function ResultPill() {
+// Props for the result pill component
+interface ResultPillProps {
+  classification?: string;
+}
+
+function ResultPill({ classification }: ResultPillProps) {
   return (
-    <div className="flex bg-red-custom p-4 w-64 justify-center border-2 border-red-light rounded-full">
-      <Bot className="mr-2" size={32} />
-      <h1 className="text-2xl">AI Generated</h1>
+    <div
+      className={`flex p-4 w-84 justify-center border-2 rounded-full ${
+        classification === "AI-Generated"
+          ? "bg-red-custom border-red-light"
+          : "bg-green-custom border-green-light"
+      }`}
+    >
+      {classification === "AI-Generated" ? (
+        <Bot className="mr-2" size={32} />
+      ) : (
+        <UserRoundCheck className="mr-2" size={32} />
+      )}
+      <h1 className="text-2xl">
+        {classification === "AI-Generated" ? "AI-Generated" : "Human-Composed"}
+      </h1>
     </div>
   );
 }
