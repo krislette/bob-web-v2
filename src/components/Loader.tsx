09115dfe475f02f1958e522ffcc5c7f94e6a0182
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-09-20 19:07:53 +0800

    Created component for the loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black-darkest">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
