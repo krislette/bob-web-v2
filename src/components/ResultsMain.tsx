bf6701a3048fc386c3f0dc654718c8e4aaaa7b27
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-10-06 22:17:58 +0800

    Used API info on display + complete components for audio & lyrics

--- src/components/ResultsMain.tsx
+++ src/components/ResultsMain.tsx
@@ -2,25 +2,22 @@ import Button from "./Button";
 import ConfidenceBar from "./ConfidenceBar";
 import ResultPill from "./ResultPill";
 import { useNavigate } from "react-router-dom";
+import AudioExplanation from "./AudioExplanation";
+import LyricsExplanation from "./LyricsExplanation";
 
 // Props for the main result body component
 interface ResultsMainProps {
-  // TODO: Change type of result once API is created
-  result?: any;
+  prediction?: any;
+  explanation?: any;
   lyrics?: string;
   fileName?: string;
 }
 
-function ResultsMain({ result, lyrics, fileName }: ResultsMainProps) {
+function ResultsMain({ prediction, explanation, fileName }: ResultsMainProps) {
   const navigate = useNavigate();
 
-  // TODO: Remove this placeholder data once API is available
-  const confidenceScore = result?.confidence || 93;
-  const classification = result?.classication || "AI-Generated";
-
-  // TODO: Use lyrics and file name if ever (like show them or whu if UI changes are permitted)
-  console.log(lyrics);
-  console.log(fileName);
+  const confidenceScore = Math.round((prediction?.probability || 0) * 100);
+  const classification = prediction?.prediction || "Unknown";
 
   const handleAnalyze = () => {
     // Just navigate back to landing page
@@ -60,26 +57,25 @@ function ResultsMain({ result, lyrics, fileName }: ResultsMainProps) {
         The top contributing factors are shown below:
       </p>
 
-      {/* XAI Results */}
-      {/* TODO: Implement this in the future cause literally I can't imagine right now since there's no API */}
-      {/* Container for the lyrics XAI results portion */}
-      <div className="flex flex-col space-y-2 p-4 w-full max-w-3xl bg-black-dark-blue min-h-96 rounded-lg">
-        <div className="flex justify-between w-full mb-4">
-          <h3>Lyrics that sounded most like AI:</h3>
-          <h3>Weight:</h3>
-        </div>
-        {/* Will probably insert component/code here for rendering XAI results */}
-      </div>
+      {/* Lyrics XAI Results */}
+      <LyricsExplanation
+        features={
+          explanation?.explanations?.filter(
+            (f: any) => f.modality === "lyrics"
+          ) || []
+        }
+        classification={classification}
+      />
 
-      {/* XAI Results - Audio */}
-      <div className="flex flex-col space-y-2 p-4 w-full max-w-3xl bg-black-dark-blue min-h-96 rounded-lg">
-        <div className="flex justify-between w-full mb-4">
-          <h3>Parts of the song that sounded most like AI:</h3>
-          <h3>Weight:</h3>
-        </div>
-
-        {/* Will probably insert component/code here for rendering XAI results */}
-      </div>
+      {/* Audio XAI Reults */}
+      <AudioExplanation
+        features={
+          explanation?.explanations?.filter(
+            (f: any) => f.modality === "audio"
+          ) || []
+        }
+        classification={classification}
+      />
 
       {/* Button */}
       <div className="flex flex-col space-y-2 w-full max-w-3xl">
