2740e542d521118bb6b3c8585c75d7edb5620101
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-10-13 13:14:37 +0800

    Updated App to include vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Results from "./pages/Results";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="min-h-screen bg-black-darkest text-white-custom">
+      {/* Main website */}
       <BrowserRouter>
         <Routes>
           <Route element={<Landing />} path="/" />
           <Route element={<Results />} path="/results" />
         </Routes>
       </BrowserRouter>
+
+      {/* Analytics */}
+      <Analytics />
     </div>
   );
 }
